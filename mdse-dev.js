0a990efd47014a0ae0bc817d7fc2addcac1d3660
Will196410 <indigo-rounds8g@icloud.com>
2026-03-06 14:05:06 +0000

    Update mdse-dev.js

--- mdse-dev.js
+++ mdse-dev.js
@@ -2146,7 +2146,7 @@ scheduleRenderStructure = makeRafScheduler(renderStructure);
 // New version: adds a tiny 10ms delay so 'blur' and 'input' finish their job first
 scheduleRenderStructure = () => {
   setTimeout(() => {
-    fastRender();
+    renderStructure();
   }, 10); 
 };
 
